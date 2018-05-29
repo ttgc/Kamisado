994efcb855c8e5df0c46b58d8bc83142f7abffce
Thomas PIOT <piott@3il.fr>
2018-05-29 16:29:50 +0200

    Beginning project

---
+++ src/kamisado/gameplay/Couleur.java
@@ -0,0 +1,13 @@
+package kamisado.gameplay;
+
+public enum Couleur {
+	Marron,
+	Orange,
+	Jaune,
+	Rouge,
+	Vert,
+	Bleu,
+	Violet,
+	Rose;
+
+}
---
+++ src/kamisado/level/MainLevel.java
@@ -0,0 +1,39 @@
+package kamisado.level;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class MainLevel extends BasicGameState {
+
+	public MainLevel() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
