bc4efbe366126384e221bfaefeb5c97aaf423f51
Thomas PIOT <piott@3il.fr>
2018-05-29 17:53:09 +0200

    updating pieces

--- src/kamisado/gameplay/Piece.java
+++ src/kamisado/gameplay/Piece.java
@@ -3,11 +3,32 @@ package kamisado.gameplay;
 public class Piece {
 	private Couleur color;
 	private Side side;
+	private PieceType type;
 
 	public Piece(Side side, Couleur color) {
 		// TODO Auto-generated constructor stub
 		this.side = side;
 		this.color = color;
+		type = PieceType.Normal;
+	}
+	
+	public boolean upgrade() {
+		switch(type) {
+		case TripleSumo:
+			return true;
+		case DoubleSumo:
+			type = PieceType.TripleSumo;
+			break;
+		case Normal:
+			type = PieceType.Sumo;
+			break;
+		case Sumo:
+			type = PieceType.DoubleSumo;
+			break;
+		default:
+			break;
+		}
+		return false;
 	}
 
 	public Couleur getColor() {
@@ -19,7 +40,7 @@ public class Piece {
 	}
 	
 	public PieceType getType() {
-		return PieceType.Normal;
+		return type;
 	}
 
 }
