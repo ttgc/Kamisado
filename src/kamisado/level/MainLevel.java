d3cc95b402b8c1b0301749776d1e280b4f2b23e7
oliviern <oliviern@s104-15.3il.fr>
2018-05-29 18:24:50 +0200

    State updated - Mouse Dragged

--- src/kamisado/level/MainLevel.java
+++ src/kamisado/level/MainLevel.java
@@ -2,10 +2,12 @@ package kamisado.level;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
+import kamisado.exceptions.MoveException;
 import kamisado.gameplay.Plateau;
 
 public class MainLevel extends BasicGameState {
@@ -24,13 +26,26 @@ public class MainLevel extends BasicGameState {
 	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
 		// TODO Auto-generated method stub
-
+		board.render(g);
+	}
+	
+	@Override
+	public void mouseDragged(int oldx, int oldy, int newx, int newy) {
+		// TODO Auto-generated method stub
+		super.mouseDragged(oldx, oldy, newx, newy);
+		if (oldx/100 == newx/100 && oldy/100 == newy/100) {
+			try {
+				board.move(oldx, oldy, newx, newy);
+			} catch (MoveException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 	@Override
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
